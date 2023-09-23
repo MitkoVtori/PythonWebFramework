543cc07be9260799a813662369834adb0d9ddee1
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2023-09-23 12:57:03 +0300

    Delete 1.Class-based views directory

--- 1.Class-based views/CBVPractice/Profile/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from CBVPractice.Profile.models import Profile
-
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("username",)
-
